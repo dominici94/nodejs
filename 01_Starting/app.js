23fa0842e4896fd8c5e2775a04607e246efd0a99
dominici94 <mattiadominici94@gmail.com>
2023-08-08 17:13:16 +0200

    routing

--- 01_Starting/app.js
+++ 01_Starting/app.js
@@ -4,27 +4,18 @@ const express = require("express");
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   console.log("In the middleware!");
-//   next(); // Permette alla richiesta di continuare al prossimo middleware
-// });
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In a middleware!");
-  // ...
-  res.send("<h1>Add Product Page!</h1>");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
-
-app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
-  // ...
-  res.send("<h1>Hello from express!</h1>");
-});
-
-// const routes = require("./routes");
-// const server = http.createServer(routes);
-
-// const server = http.createServer(app);
-// server.listen(3000);
 
 app.listen(3000);
