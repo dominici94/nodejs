5a0aee646abe89113f880e29a7b7ae6a06f3e5a2
dominici94 <mattiadominici94@gmail.com>
2023-08-09 12:18:05 +0200

    attach static files

--- 01_Starting/app.js
+++ 01_Starting/app.js
@@ -1,4 +1,5 @@
 // const http = require("http");
+const path = require("path");
 
 const express = require("express");
 
@@ -10,12 +11,15 @@ const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
 });
 
 app.listen(3000);
