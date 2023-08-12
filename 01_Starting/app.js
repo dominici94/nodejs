ebbf7bb5f7e74d698289c228ea604190e6f6cf9a
dominici94 <mattiadominici94@gmail.com>
2023-08-12 19:42:44 +0200

    template engine pug

--- 01_Starting/app.js
+++ 01_Starting/app.js
@@ -5,7 +5,10 @@ const express = require("express");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const bodyParser = require("body-parser");
@@ -13,13 +16,12 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
+  res.status(404).render("page-not-found");
+  // .sendFile(path.join(__dirname, "views", "page-not-found.html"));
 });
 
 app.listen(3000);
