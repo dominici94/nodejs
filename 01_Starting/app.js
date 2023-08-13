098a3676e4ba79212e2e948fe1b3d19ac7a1aa9c
dominici94 <mattiadominici94@gmail.com>
2023-08-13 09:52:06 +0200

    add layout with pug

--- 01_Starting/app.js
+++ 01_Starting/app.js
@@ -3,8 +3,10 @@ const path = require("path");
 
 const express = require("express");
 
+// Utilizzo di applicazione con express
 const app = express();
 
+// Utilizzo di pug come template engine e va a prendere i file nella cartella views
 app.set("view engine", "pug");
 app.set("views", "views");
 
@@ -16,11 +18,15 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+//Utilizzo di rotte admin e shop
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
+// Rotta finale diversa da quelle dei file admin e shop che porta alla pagina di errore
 app.use((req, res, next) => {
-  res.status(404).render("page-not-found");
+  res
+    .status(404)
+    .render("page-not-found", { docTitle: "404: Page Not Found!" });
   // .sendFile(path.join(__dirname, "views", "page-not-found.html"));
 });
 
--- 01_Starting/routes/admin.js
+++ 01_Starting/routes/admin.js
@@ -10,14 +10,15 @@ const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product");
+  res.render("add-product", {
+    docTitle: "Add Product",
+    path: "/admin/add-product",
+  });
   // res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  // console.log(req.body.title);
-  // console.log(req.body);
   products.push({
     title: req.body.title,
   });
