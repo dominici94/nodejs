6d4c0a2f1c823066bb622eb894572e2e510c12a4
dominici94 <mattiadominici94@gmail.com>
2023-08-22 18:10:59 +0200

    add orders

--- 01_Starting/controllers/shop.js
+++ 01_Starting/controllers/shop.js
@@ -30,6 +30,10 @@ exports.getCart = (req, res, next) => {
   res.render("shop/cart", { path: "/cart", docTitle: "Your Cart" });
 };
 
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", { path: "/orders", docTitle: "Your Orders" });
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", { path: "/checkout", docTitle: "Checkout" });
 };
