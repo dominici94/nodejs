73067d4e2fded1ddb544c756d420e55a59494633
dominici94 <mattiadominici94@gmail.com>
2023-08-29 15:28:24 +0200

    delete cart, product

--- 01_Starting/controllers/admin.js
+++ 01_Starting/controllers/admin.js
@@ -25,7 +25,7 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const price = req.body.price;
 
-  const prod = new Product(title, imageUrl, description, price);
+  const prod = new Product(null, title, imageUrl, description, price);
   prod.save();
 
   res.redirect("/");
@@ -50,6 +50,23 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDescription,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect("/admin/products");
+};
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("admin/products", {
@@ -58,4 +75,12 @@ exports.getProducts = (req, res, next) => {
       path: "/admin/products",
     });
   });
+};
+
+exports.deleteProduct = (req, res, next) => {
+  const id = req.body.prodId;
+
+  Product.deleteById(id);
+
+  res.redirect("/admin/products");
 };
